d5bb7712d9e9b8a3be4bd22274b4e2ec16bcb350
liuzijian <liuzijian@gaohangip.com>
2020-07-22 14:21:36 +0800

    更新gulp

--- gulpfile.js
+++ gulpfile.js
@@ -24,12 +24,21 @@ var env         = ''; // 运行环境
 gulp.task('build', $.shell.task([ 'node build/build.js' ]));
 
 // 创建版本号目录
+// gulp.task('create:versionCatalog', gulp.series('build', function () {
+//   return gulp.src(`${distPath}/static/**/*`)
+//     .pipe(gulp.dest(`${versionPath}/static/`))
+// }));
 gulp.task('create:versionCatalog', ['build'], function () {
   return gulp.src(`${distPath}/static/**/*`)
     .pipe(gulp.dest(`${versionPath}/static/`))
 });
 
 // 替换${versionPath}/static/js/manifest.js window.SITE_CONFIG.cdnUrl占位变量
+// gulp.task('replace:cdnUrl', gulp.series('create:versionCatalog', function () {
+//   return gulp.src(`${versionPath}/static/js/manifest.js`)
+//     .pipe($.replace(new RegExp(`"${require('./config').build.assetsPublicPath}"`, 'g'), 'window.SITE_CONFIG.cdnUrl + "/"'))
+//     .pipe(gulp.dest(`${versionPath}/static/js/`))
+// }));
 gulp.task('replace:cdnUrl', ['create:versionCatalog'], function () {
   return gulp.src(`${versionPath}/static/js/manifest.js`)
     .pipe($.replace(new RegExp(`"${require('./config').build.assetsPublicPath}"`, 'g'), 'window.SITE_CONFIG.cdnUrl + "/"'))
@@ -37,6 +46,11 @@ gulp.task('replace:cdnUrl', ['create:versionCatalog'], function () {
 });
 
 // 替换${versionPath}/static/config/index-${env}.js window.SITE_CONFIG['version']配置变量
+// gulp.task('replace:version', gulp.series('create:versionCatalog', function () {
+//   return gulp.src(`${versionPath}/static/config/index-${env}.js`)
+//     .pipe($.replace(/window.SITE_CONFIG\['version'\] = '.*'/g, `window.SITE_CONFIG['version'] = '${version}'`))
+//     .pipe(gulp.dest(`${versionPath}/static/config/`))
+// }));
 gulp.task('replace:version', ['create:versionCatalog'], function () {
   return gulp.src(`${versionPath}/static/config/index-${env}.js`)
     .pipe($.replace(/window.SITE_CONFIG\['version'\] = '.*'/g, `window.SITE_CONFIG['version'] = '${version}'`))
@@ -44,6 +58,11 @@ gulp.task('replace:version', ['create:versionCatalog'], function () {
 });
 
 // 合并${versionPath}/static/config/[index-${env}, init].js 至 ${distPath}/config/index.js
+// gulp.task('concat:config', gulp.series('replace:version', function () {
+//   return gulp.src([`${versionPath}/static/config/index-${env}.js`, `${versionPath}/static/config/init.js`])
+//     .pipe($.concat('index.js'))
+//     .pipe(gulp.dest(`${distPath}/config/`))
+// }));
 gulp.task('concat:config', ['replace:version'], function () {
   return gulp.src([`${versionPath}/static/config/index-${env}.js`, `${versionPath}/static/config/init.js`])
     .pipe($.concat('index.js'))
@@ -55,13 +74,15 @@ gulp.task('clean', function () {
   return del([versionPath])
 });
 
-gulp.task('default', ['clean'], function () {
+gulp.task('default', gulp.series('clean', function () {
   // 获取环境配置
   env = process.env.npm_config_qa ? 'qa' : process.env.npm_config_uat ? 'uat' : 'prod'
 
   // 开始打包编译
+
   gulp.start(['build', 'create:versionCatalog', 'replace:cdnUrl', 'replace:version', 'concat:config'], function () {
     // 清除, 编译 / 处理项目中产生的文件
     del([`${distPath}/static`, `${versionPath}/static/config`])
   })
-});
+}));
+
