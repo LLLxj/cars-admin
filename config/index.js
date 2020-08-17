dea480cca6d6b5aba059ea6f078903fc5fdc99b9
liuzijian <liuzijian@gaohangip.com>
2020-08-17 19:50:58 +0800

    更新

--- config/index.js
+++ config/index.js
@@ -38,8 +38,8 @@ module.exports = {
     //   }
     // },
     // Various Dev Server settings
-    // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '0.0.0.0', // can be overwritten by process.env.HOST
     // host: '192.168.1.107', // can be overwritten by process.env.HOST
     port: 8001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
