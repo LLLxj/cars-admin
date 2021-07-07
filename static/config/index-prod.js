243f25a5eae7d3df7d644fb4cb4a52191a759322
liuzijian <253358001@qq.com>
2021-07-07 10:29:18 +0800

    fix环境配置

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {}
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://39.100.108.10:8086/'
+  window.SITE_CONFIG['baseUrl'] = 'http://47.106.102.93:8086/'
   // window.SITE_CONFIG['baseUrl'] = 'https://admin.school.heigrace.com/api/'
 
   // cdn地址 = 域名 + 版本号
